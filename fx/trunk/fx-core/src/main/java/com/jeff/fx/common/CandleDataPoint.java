fe542a7efde854f4782d65d4b05d8620a6072214
jeffcann <jeffcann@360c4b46-674e-11de-9b35-9190732fe600>
2009-09-23 14:18:43 +0000

    Added CandleValueModel to evaluate candles to single values

--- fx/trunk/fx-core/src/main/java/com/jeff/fx/common/CandleDataPoint.java
+++ fx/trunk/fx-core/src/main/java/com/jeff/fx/common/CandleDataPoint.java
@@ -2,33 +2,30 @@ package com.jeff.fx.common;
 
 import java.io.Serializable;
 
-public class CandleDataPoint extends AbstractFXDataPoint implements Serializable
-{
+public class CandleDataPoint extends AbstractFXDataPoint implements
+		Serializable {
 	private static final long serialVersionUID = -1074171723478712229L;
 
 	private Period period;
-	
+
 	private double buyOpen;
 	private double buyHigh;
 	private double buyLow;
 	private double buyClose;
-	
+
 	private double sellOpen;
 	private double sellHigh;
 	private double sellLow;
 	private double sellClose;
-	
+
 	private int tickCount;
 
-	
-	public CandleDataPoint()
-	{
+	public CandleDataPoint() {
 	}
 
-	public CandleDataPoint(CandleDataPoint candle)
-	{
+	public CandleDataPoint(CandleDataPoint candle) {
 		super(candle);
-		
+
 		this.period = candle.period;
 		this.buyClose = candle.buyClose;
 		this.buyHigh = candle.buyHigh;
@@ -40,36 +37,43 @@ public class CandleDataPoint extends AbstractFXDataPoint implements Serializable
 		this.sellOpen = candle.sellOpen;
 		this.tickCount = candle.tickCount;
 	}
-	
+
 	@Override
-	public String toString()
-	{
-		return String.format("[%s/%s/%s] %s / %.4f %.4f %.4f %.4f (%d ticks)", getInstrument(), getDataSource(), period.key, getDate(), buyOpen, buyHigh, buyLow, buyClose, tickCount);
+	public String toString() {
+		return String.format("[%s/%s/%s] %s / %.4f %.4f %.4f %.4f (%d ticks)",
+				getInstrument(), getDataSource(), period.key, getDate(),
+				buyOpen, buyHigh, buyLow, buyClose, tickCount);
 	}
-	
-	public void merge(CandleDataPoint newClose)
-	{
+
+	public double evaluate(CandleValueModel model) {
+		return model.evaluate(this);
+	}
+
+	public void merge(CandleDataPoint newClose) {
 		buyClose = newClose.buyClose;
 		sellClose = newClose.sellClose;
-		
-		if(newClose.buyHigh > buyHigh) buyHigh = newClose.buyHigh;
-		if(newClose.sellHigh > sellHigh) sellHigh = newClose.sellHigh;
-		
-		if(newClose.buyLow < buyLow) buyLow = newClose.buyLow;
-		if(newClose.sellLow < sellLow) sellLow = newClose.sellLow;
-		
+
+		if (newClose.buyHigh > buyHigh)
+			buyHigh = newClose.buyHigh;
+		if (newClose.sellHigh > sellHigh)
+			sellHigh = newClose.sellHigh;
+
+		if (newClose.buyLow < buyLow)
+			buyLow = newClose.buyLow;
+		if (newClose.sellLow < sellLow)
+			sellLow = newClose.sellLow;
+
 		tickCount += newClose.tickCount;
 	}
-	
+
 	/**
 	 * Convert the candle to a tick (based on opening price)
 	 * 
 	 * @return
 	 */
-	public TickDataPoint toTick()
-	{
+	public TickDataPoint toTick() {
 		TickDataPoint tick = new TickDataPoint();
-		
+
 		tick.setBuy(getBuyOpen());
 		tick.setDataSource(getDataSource());
 		tick.setDate(getDate());
@@ -77,144 +81,121 @@ public class CandleDataPoint extends AbstractFXDataPoint implements Serializable
 		tick.setSell(getSellOpen());
 		tick.setBuyVolume(getBuyVolume());
 		tick.setSellVolume(getSellVolume());
-		
+
 		return tick;
 	}
-	
-	public void setApproximatedValues(CandleDataPoint candle, boolean useCloseTime)
-	{
+
+	public void setApproximatedValues(CandleDataPoint candle,
+			boolean useCloseTime) {
 		setPeriod(candle.getPeriod());
 		setInstrument(candle.getInstrument());
 		setDataSource(candle.getDataSource());
-		
-		if(useCloseTime)
-		{
+
+		if (useCloseTime) {
 			buyOpen = candle.getBuyClose();
 			buyClose = candle.getBuyClose();
 			buyHigh = candle.getBuyClose();
 			buyLow = candle.getBuyClose();
-			
+
 			sellOpen = candle.getSellClose();
 			sellHigh = candle.getSellClose();
 			sellLow = candle.getSellClose();
 			sellClose = candle.getSellClose();
-		}
-		else
-		{
+		} else {
 			buyOpen = candle.getBuyOpen();
 			buyClose = candle.getBuyOpen();
 			buyHigh = candle.getBuyOpen();
 			buyLow = candle.getBuyOpen();
-			
+
 			sellOpen = candle.getSellOpen();
 			sellHigh = candle.getSellOpen();
 			sellLow = candle.getSellOpen();
 			sellClose = candle.getSellOpen();
 		}
-		
+
 		setBuyVolume(0);
 		setSellVolume(0);
 
 		tickCount = 0;
 	}
-	
-	public double getBuyOpen()
-	{
+
+	public double getBuyOpen() {
 		return buyOpen;
 	}
 
-	public void setBuyOpen(double buyOpen)
-	{
+	public void setBuyOpen(double buyOpen) {
 		this.buyOpen = buyOpen;
 	}
 
-	public double getBuyHigh()
-	{
+	public double getBuyHigh() {
 		return buyHigh;
 	}
 
-	public void setBuyHigh(double buyHigh)
-	{
+	public void setBuyHigh(double buyHigh) {
 		this.buyHigh = buyHigh;
 	}
 
-	public double getBuyLow()
-	{
+	public double getBuyLow() {
 		return buyLow;
 	}
 
-	public void setBuyLow(double buyLow)
-	{
+	public void setBuyLow(double buyLow) {
 		this.buyLow = buyLow;
 	}
 
-	public double getBuyClose()
-	{
+	public double getBuyClose() {
 		return buyClose;
 	}
 
-	public void setBuyClose(double buyClose)
-	{
+	public void setBuyClose(double buyClose) {
 		this.buyClose = buyClose;
 	}
 
-	public double getSellOpen()
-	{
+	public double getSellOpen() {
 		return sellOpen;
 	}
 
-	public void setSellOpen(double sellOpen)
-	{
+	public void setSellOpen(double sellOpen) {
 		this.sellOpen = sellOpen;
 	}
 
-	public double getSellHigh()
-	{
+	public double getSellHigh() {
 		return sellHigh;
 	}
 
-	public void setSellHigh(double sellHigh)
-	{
+	public void setSellHigh(double sellHigh) {
 		this.sellHigh = sellHigh;
 	}
 
-	public double getSellLow()
-	{
+	public double getSellLow() {
 		return sellLow;
 	}
 
-	public void setSellLow(double sellLow)
-	{
+	public void setSellLow(double sellLow) {
 		this.sellLow = sellLow;
 	}
 
-	public double getSellClose()
-	{
+	public double getSellClose() {
 		return sellClose;
 	}
 
-	public void setSellClose(double sellClose)
-	{
+	public void setSellClose(double sellClose) {
 		this.sellClose = sellClose;
 	}
 
-	public Period getPeriod()
-    {
-    	return period;
-    }
+	public Period getPeriod() {
+		return period;
+	}
 
-	public void setPeriod(Period period)
-    {
-    	this.period = period;
-    }
+	public void setPeriod(Period period) {
+		this.period = period;
+	}
 
-	public int getTickCount()
-	{
+	public int getTickCount() {
 		return tickCount;
 	}
 
-	public void setTickCount(int tickCount)
-	{
+	public void setTickCount(int tickCount) {
 		this.tickCount = tickCount;
 	}
 }
