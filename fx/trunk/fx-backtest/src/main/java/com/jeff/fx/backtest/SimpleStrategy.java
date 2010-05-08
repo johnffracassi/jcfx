0d74824f410f9a69d3f2cdd836220b5b3478fc07
jeffcann <jeffcann@360c4b46-674e-11de-9b35-9190732fe600>
2010-05-08 15:39:00 +0000

    First successful implementation for creating parameter value table

--- fx/trunk/fx-backtest/src/main/java/com/jeff/fx/backtest/SimpleStrategy.java
+++ fx/trunk/fx-backtest/src/main/java/com/jeff/fx/backtest/SimpleStrategy.java
@@ -1,5 +1,6 @@
 package com.jeff.fx.backtest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,10 +17,82 @@ public class SimpleStrategy {
 	private int candleCount = 0;
 	private BTOrder currentOrder = null;
 	
+	public static void main(String[] args) {
+		
+		BTParameterSet pbs = new BTParameterSet();
+		
+		BTParameterValueSet pvs1 = new BTParameterValueSet();
+		pvs1.setStartValue(0.01);
+		pvs1.setEndValue(0.25);
+		pvs1.setStep(0.02);
+		
+		BTParameterValueSet pvs2 = new BTParameterValueSet();
+		pvs2.setStartValue(1);
+		pvs2.setEndValue(5);
+		pvs2.setStep(1);
+		
+		BTParameterValueSet pvs3 = new BTParameterValueSet();
+		pvs3.setStartValue(1);
+		pvs3.setEndValue(2);
+		pvs3.setStep(.5);
+		
+		pbs.setParameter("p1", pvs1);
+		pbs.setParameter("p2", pvs2);
+		pbs.setParameter("p3", pvs3);
+		
+		System.out.println(pbs);
+		
+		double[][] valueTable = getParameterValueTable(pbs);
+	}
+	
+	public static double[][] getParameterValueTable(BTParameterSet ps) {
+		
+		int[] cycles = new int[ps.getKeyCount()];
+		for(int i=0; i<cycles.length; i++) {
+			cycles[i] = ps.getValueSet(i).getNumberOfSteps();
+		}
+		
+		int[] repeats = new int[ps.getKeyCount()];
+		repeats[repeats.length-1] = 1;
+		for(int i=repeats.length-2; i>=0; i--) {
+			repeats[i] = repeats[i+1] * cycles[i+1];
+		}
+
+		int permutations = cycles[0] * cycles[1] * cycles[2];
+		double[][] data = new double[cycles.length][permutations];
+		
+		BTParameterValueSet[] pvs = new BTParameterValueSet[ps.getKeyCount()];
+		for(int i=0; i<pvs.length; i++) {
+			pvs[i] = ps.getValueSet(i);
+		}
+		
+		for(int pvsIdx=0; pvsIdx<cycles.length; pvsIdx++) {
+			int permutation = 0;
+			int idxInDataset = 0;
+			double[] dataset = pvs[pvsIdx].expand();
+
+			while(permutation < permutations) {
+				
+				for(int repeat=0; repeat<repeats[pvsIdx]; repeat++) {
+					data[pvsIdx][permutation] = dataset[idxInDataset];
+					permutation++;
+				}
+
+				idxInDataset ++;
+				if(idxInDataset == cycles[pvsIdx]) {
+					idxInDataset = 0;
+				}
+			}
+		}
+		
+		return data;
+	}
+	
 	public BTResult test(List<CandleDataPoint> candles, BTParameterSet param) {
 		for(CandleDataPoint candle : candles) {
 			candle(candle);
 		}
+		return new BTResult();
 	}
 
 	private void candle(CandleDataPoint candle) {
@@ -31,7 +104,7 @@ public class SimpleStrategy {
 			close(currentOrder, candle);
 		}
 	}
-
+	
 	private void open(BTOrder order, CandleDataPoint candle) {
 		order.setOpenTime(candle.getDate());
 		order.setOpenPrice(order.getOfferSide() == OfferSide.Ask ? candle.getBuyOpen() : candle.getSellOpen());
@@ -124,8 +197,14 @@ class BTResult {
 	}
 }
 
+class BTParameterTable {
+	public BTParameterTable(BTParameterSet ps) {
+	}
+}
+
 class BTParameterSet {
 
+	private List<String> keys = new ArrayList<String>(10);
 	private Map<String, BTParameterValueSet> params = new HashMap<String, BTParameterValueSet>();
 
 	public int getPermutationCount() {
@@ -145,36 +224,42 @@ class BTParameterSet {
 		return perms;
 	}
 	
-	public Set<String> getKeys() {
-		return params.keySet();
+	public BTParameterValueSet getValueSet(int idx) {
+		return params.get(keys.get(idx));
+	}
+	
+	public BTParameterValueSet getValueSet(String key) {
+		return params.get(key);
+	}
+	
+	public List<String> getKeys() {
+		return keys;
 	}
 
 	public int getKeyCount() {
-		return params.keySet().size();
+		return keys.size();
 	}
 	
 	public void setParameter(String key, BTParameterValueSet val) {
 		params.put(key, val);
+		keys.add(key);
 	}
 
 	public void removeParameter(String key) {
 		params.remove(key);
+		keys.remove(key);
 	}
-}
-
-class BTParameterTable {
 	
-	private double[][] values;
-	
-	public BTParameterTable(BTParameterSet params) {
+	@Override
+	public String toString() {
+		StringBuffer buf = new StringBuffer();
 		
-		values = new double[params.getKeyCount()][params.getPermutationCount()];
-			
-		for(String key : params.getKeys()) {
-			
+		for(String key : keys) {
+			buf.append(key + "=" + params.get(key) + "\n");
 		}
+		
+		return buf.toString();
 	}
-	
 }
 
 class BTParameterValueSet {
@@ -184,9 +269,19 @@ class BTParameterValueSet {
 	private double step;
 
 	public int getNumberOfSteps() {
-		return (int) ((endValue - startValue) / step);
+		return (int) ((endValue - startValue) / step) + 1;
 	}
 
+	public double[] expand() {
+		double[] values = new double[getNumberOfSteps()];
+		
+		for(int i=0; i<values.length; i++) {
+			values[i] = startValue + ((double)i * (double)step);
+		}
+		
+		return values;
+	}
+	
 	public double getValue(int step) {
 		return (startValue + ((endValue - startValue) / (double) step));
 	}
@@ -213,5 +308,21 @@ class BTParameterValueSet {
 
 	public void setStep(double step) {
 		this.step = step;
+	}
+	
+	@Override
+	public String toString() {
+		return "[" + startValue + "=>" + endValue + " s=" + step + " " + toString(expand()) + "]"; 
+	}
+	
+	private String toString(double[] vals) {
+		String str = "";
+		for(int i=0; i<vals.length; i++) {
+			str += String.valueOf(vals[i]);
+			if(i <vals.length-1) {
+				str += ", ";
+			}
+		}
+		return "<" + str + ">";
 	}
 }
