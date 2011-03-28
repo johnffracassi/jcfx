37002374b7c9d74b664bd438f3b603e9331e1c00
jeffcann <jeffcann@360c4b46-674e-11de-9b35-9190732fe600>
2011-03-28 11:32:03 +0000

    Added files

--- fx/trunk/fx-backtest/src/main/java/com/jeff/fx/graph/node/TimeRangeNode.java
+++ fx/trunk/fx-backtest/src/main/java/com/jeff/fx/graph/node/TimeRangeNode.java
@@ -1,9 +1,23 @@
 package com.jeff.fx.graph.node;
 
+import com.jeff.fx.common.CandleDataPoint;
+import com.jeff.fx.common.TimeOfWeek;
+import com.jeff.fx.lookforward.CandleFilterModel;
+
 public class TimeRangeNode extends BaseNode
 {
+    private TimeOfWeek start;
+    private TimeOfWeek end;
+
     public TimeRangeNode()
     {
         super("TimeRange");
+    }
+
+    @Override
+    public boolean evaluate(CandleDataPoint candle, CandleFilterModel model)
+    {
+        System.out.println("Checking time range?");
+        return true;
     }
 }
