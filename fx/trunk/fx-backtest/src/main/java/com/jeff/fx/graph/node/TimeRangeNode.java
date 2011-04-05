24340cc61899b0a2f1dedcd0d64876026c753e73
jeffcann <jeffcann@360c4b46-674e-11de-9b35-9190732fe600>
2011-04-05 03:16:39 +0000

    Working example with time range node

--- fx/trunk/fx-backtest/src/main/java/com/jeff/fx/graph/node/TimeRangeNode.java
+++ fx/trunk/fx-backtest/src/main/java/com/jeff/fx/graph/node/TimeRangeNode.java
@@ -12,12 +12,20 @@ public class TimeRangeNode extends BaseNode
     public TimeRangeNode()
     {
         super("TimeRange");
+
+        this.start = new TimeOfWeek(TimeOfWeek.MONDAY, 10, 30);
+        this.end = new TimeOfWeek(TimeOfWeek.MONDAY, 11, 30);
     }
 
     @Override
     public boolean evaluate(CandleDataPoint candle, CandleFilterModel model)
     {
-        System.out.println("Checking time range?");
-        return (true);
+        TimeOfWeek time = new TimeOfWeek(candle.getDateTime());
+        return time.isAfterOrEqualTo(start) && time.isBefore(end);
+    }
+
+    @Override
+    public String getLabel() {
+        return "Between\n" + start + " and\n" + end;
     }
 }
