542ebd6a0a5ea6951d8bbcebd7463a4f7490e7fe
jeffcann <jeffcann@360c4b46-674e-11de-9b35-9190732fe600>
2011-05-11 13:00:57 +0000

    Improvements to look forward filtering and charting

--- fx/trunk/fx-backtest/src/main/java/com/jeff/fx/filter/LookForwardChartController.java
+++ fx/trunk/fx-backtest/src/main/java/com/jeff/fx/filter/LookForwardChartController.java
@@ -1,19 +1,10 @@
 package com.jeff.fx.filter;
 
-import com.jeff.fx.backtest.chart.IndicatorDataset;
-import com.jeff.fx.backtest.strategy.IndicatorCache;
-import com.jeff.fx.common.*;
-import com.jeff.fx.datastore.CandleDataStore;
+import com.jeff.fx.common.CandleDataPoint;
 import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
-import org.joda.time.LocalDate;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.springframework.stereotype.Component;
 
-import java.awt.*;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 @Component
@@ -28,16 +19,21 @@ public class LookForwardChartController
 
     public void setCollections(List<List<CandleDataPoint>> collections) {
 
-        view.setChart(FourPointValueChart.createChart(collections));
+        view.setChart(MultiSeriesValueChart.createChart());
+
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer(true, false);
 
         XYPlot plot = (XYPlot) view.getChart().getPlot();
+
         for (List<CandleDataPoint> collection : collections) {
-            plot.setDataset(plot.getDatasetCount(), new FourPointValueDataset(collection));
-            plot.setRenderer(plot.getDatasetCount(), new StandardXYItemRenderer());
+//            plot.setDataset(plot.getDatasetCount(), new FourPointValueDataset(collection));
+            plot.setDataset(plot.getDatasetCount(), new AllPointsValueDataset(collection));
+            plot.setRenderer(plot.getDatasetCount(), renderer);
         }
     }
 
-    public LookForwardChartView getView() {
+    public LookForwardChartView getView()
+    {
         return view;
     }
 }
--- fx/trunk/fx-backtest/src/main/java/com/jeff/fx/filter/LookForwardController.java
+++ fx/trunk/fx-backtest/src/main/java/com/jeff/fx/filter/LookForwardController.java
@@ -1,6 +1,5 @@
 package com.jeff.fx.filter;
 
-import com.jeff.fx.backtest.strategy.IndicatorCache;
 import com.jeff.fx.common.*;
 import com.jeff.fx.datastore.CandleDataStore;
 import org.joda.time.LocalDate;
@@ -8,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 @Component
@@ -24,93 +21,16 @@ public class LookForwardController {
     @Autowired
     private CandleDataStore loader;
 
-    @Autowired
-    private CandleFilterModelEvaluator evaluator;
-
+    private CandleCollection candles;
 
     public LookForwardController()
     {
     }
 
-    public void run()
+    public void updateDataset(List<List<CandleDataPoint>> collections)
     {
-        try
-        {
-            CandleCollection candles = loadTestData();
-            List<CandleDataPoint> startPoints = findStartPoints(candles, new TimeOfWeek("Mo0845"));
-            List<List<CandleDataPoint>> collections = findIndividualPriceLines(candles, startPoints, 32);
-
-            datasetController.setCollections(collections);
-            chartController.setCollections(collections);
-        }
-        catch(Exception ex)
-        {
-            ex.printStackTrace();
-        }
-    }
-
-    private List<List<CandleDataPoint>> findIndividualPriceLines(CandleCollection candles, List<CandleDataPoint> startPoints, int lookAheadDistance)
-    {
-        List<List<CandleDataPoint>> collections = new ArrayList<List<CandleDataPoint>>(startPoints.size());
-
-        for(CandleDataPoint startPoint : startPoints)
-        {
-            int idx = candles.getCandleIndex(startPoint.getDateTime());
-            List<CandleDataPoint> collection = candles.getCandles(idx, lookAheadDistance);
-            collections.add(collection);
-        }
-
-        return collections;
-    }
-
-    private List<CandleDataPoint> findStartPoints(CandleCollection candles, String expression)
-    {
-        List<CandleDataPoint> list = new LinkedList<CandleDataPoint>();
-
-        long stime = System.nanoTime();
-
-        CandleFilterModel model = new CandleFilterModel(candles, new IndicatorCache(), evaluator);
-
-        for(int c=0; c<candles.getCandleCount(); c++)
-        {
-            model.setIndex(c);
-            CandleDataPoint candle = model.getCandles().getCandle(c);
-            boolean result = evaluator.evaluate(model, expression, boolean.class);
-
-            if(result)
-            {
-                list.add(candle);
-            }
-        }
-
-        System.out.printf("Completed in %.3fs (selected %d of %d candles)", (System.nanoTime() - stime) / 1000000000.0, list.size(), candles.getCandleCount());
-
-        return list;
-    }
-
-    private List<CandleDataPoint> findStartPoints(CandleCollection candles, TimeOfWeek time)
-    {
-        List<CandleDataPoint> list = new LinkedList<CandleDataPoint>();
-
-        long stime = System.nanoTime();
-
-        CandleFilterModel model = new CandleFilterModel(candles, new IndicatorCache(), evaluator);
-
-        for(int c=0; c<candles.getCandleCount(); c++)
-        {
-            model.setIndex(c);
-            CandleDataPoint candle = model.getCandles().getCandle(c);
-            boolean result = new TimeOfWeek(candle.getDateTime()).equals(time);
-
-            if(result)
-            {
-                list.add(candle);
-            }
-        }
-
-        System.out.printf("Completed in %.3fs (selected %d of %d candles)", (System.nanoTime() - stime) / 1000000000.0, list.size(), candles.getCandleCount());
-
-        return list;
+        datasetController.setCollections(collections);
+        chartController.setCollections(collections);
     }
 
     private CandleCollection loadTestData() throws IOException
@@ -130,4 +50,21 @@ public class LookForwardController {
         return loader.loadCandles(request).getCandles();
     }
 
+    public CandleCollection loadCandles()
+    {
+        try
+        {
+            candles = loadTestData();
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return candles;
+    }
+
+    public CandleCollection getCandles()
+    {
+        return (candles == null) ? loadCandles() : candles;
+    }
 }
