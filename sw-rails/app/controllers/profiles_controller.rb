d2fee75e0e201afba0349ac3c5f8527d4d665276
jeffcann <jeffcann@360c4b46-674e-11de-9b35-9190732fe600>
2011-05-31 11:53:41 +0000

    Work on player profiles

--- sw-rails/app/controllers/profiles_controller.rb
+++ sw-rails/app/controllers/profiles_controller.rb
@@ -1,46 +1,33 @@
 class ProfilesController < ApplicationController
-  # GET /profiles
-  # GET /profiles.xml
+
   def index
     @profiles = Profile.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @profiles }
-    end
   end
 
   def show
     @profile = Profile.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @profile }
-    end
   end
 
-  def stats
+  def career
     @profile = Profile.find(params[:id])
   end
 
-  # GET /profiles/new
-  # GET /profiles/new.xml
-  def new
-    @profile = Profile.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @profile }
-    end
+  def batting
+    @profile = Profile.find(params[:id])
   end
 
-  # GET /profiles/1/edit
+  def bowling
+    @profile = Profile.find(params[:id])
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
   def edit
     @profile = Profile.find(params[:id])
   end
 
-  # POST /profiles
-  # POST /profiles.xml
   def create
     @profile = Profile.new(params[:profile])
 
@@ -55,8 +42,6 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # PUT /profiles/1
-  # PUT /profiles/1.xml
   def update
     @profile = Profile.find(params[:id])
 
@@ -68,18 +53,6 @@ class ProfilesController < ApplicationController
         format.html { render :action => "edit" }
         format.xml  { render :xml => @profile.errors, :status => :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /profiles/1
-  # DELETE /profiles/1.xml
-  def destroy
-    @profile = Profile.find(params[:id])
-    @profile.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(profiles_url) }
-      format.xml  { head :ok }
     end
   end
 end
