6621a5fb1b2989997c2e7fd84e88963a01145ff3
jeffcann <jeffcann@360c4b46-674e-11de-9b35-9190732fe600>
2011-06-18 14:35:53 +0000

    Changed umpire animations to One Shot

--- web-cricket/public/javascripts/data/sprites.js
+++ web-cricket/public/javascripts/data/sprites.js
@@ -94,11 +94,11 @@ anim['Bowler_Running']      = anim['Fielder_Running'];
 anim['Bowler_Action']       = new Animation(1, [[3,0],[3,0],[3,1],[3,1],[3,2],[3,2],[3,3],[3,4],[3,5],[1,2]], [1,0]);
 
 anim['Umpire_Idle']         = new SingleFrame([2,0]);
-anim['Umpire_SignalFour']   = new Animation(4, [[2,2],[2,2],[2,2],[2,2],[2,3],[2,3],[2,3],[2,3]], [2,0]);
-anim['Umpire_SignalSix']    = new SingleFrame([2,1]);
-anim['Umpire_SignalWide']   = new SingleFrame([2,5]);
-anim['Umpire_SignalNoBall'] = new SingleFrame([2,3]);
-anim['Umpire_SignalOut']    = new SingleFrame([2,8]);
-anim['Umpire_SignalBye']    = new SingleFrame([2,8]);
-anim['Umpire_SignalLegBye'] = new Animation(6, [[2,7],[2,7],[2,7],[2,7],[2,6],[2,6],[2,6],[2,6]], [2,0]);
+anim['Umpire_SignalFour']   = new Animation(5, [[2,2],[2,2],[2,2],[2,2],[2,3],[2,3],[2,3],[2,3]], [2,0]);
+anim['Umpire_SignalSix']    = new Animation(40, [[2,1]], [2,0]);
+anim['Umpire_SignalWide']   = new Animation(40, [[2,5]], [2,0]);
+anim['Umpire_SignalNoBall'] = new Animation(40, [[2,3]], [2,0]);
+anim['Umpire_SignalOut']    = new Animation(40, [[2,8]], [2,0]);
+anim['Umpire_SignalBye']    = new Animation(40, [[2,8]], [2,0]);
+anim['Umpire_SignalLegBye'] = new Animation(5, [[2,7],[2,7],[2,7],[2,7],[2,6],[2,6],[2,6],[2,6]], [2,0]);
 
