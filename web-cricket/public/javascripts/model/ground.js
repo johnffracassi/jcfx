4ce0c6e710291aa14c0f6529d0c7191880d2bee9
jeffcann <jeffcann@360c4b46-674e-11de-9b35-9190732fe600>
2011-07-19 12:04:46 +0000

    Added boundary checking

--- web-cricket/public/javascripts/model/ground.js
+++ web-cricket/public/javascripts/model/ground.js
@@ -1,16 +1,59 @@
 var GroundModel = Class.extend({
+
     straightLength: 75,
     squareLength: 65,
     sections: 64,
     fillColour: "rgb(30, 140, 0)",
     bounce: 0.2,
-    roll: 1.0
+    roll: 1.0,
+    poly: null,
+    fieldingCirclePoly: null,
+
+    init: function()
+    {
+        this.poly = calculateEllipse(0, 10, this.squareLength, this.straightLength, 0, this.sections);
+        this.fieldingCirclePoly = calculateEllipse(0, 10, 26, 40, 0, 36);
+    },
+
+    boundContains: function(pt)
+    {
+        if(pt[0] > (this.straightLength) + 10 || pt[0] < -(this.straightLength - 10))
+        {
+            return false;
+        }
+
+        if(pt[1] > this.squareLength || pt[1] < -this.squareLength)
+        {
+            return false;
+        }
+
+        return true;
+    },
+
+    contains: function(pt)
+    {
+        if(!this.boundContains(pt))
+        {
+            return false;
+        }
+
+        var i, j, c = false;
+        for (i = 0, j = this.poly.length - 1; i < this.poly.length; j = i++)
+        {
+            if (((this.poly[i][1] > pt[1]) != (this.poly[j][1] > pt[1])) && (pt[0] < (this.poly[j][0] - this.poly[i][0]) * (pt[1] - this.poly[i][1]) / (this.poly[j][1] - this.poly[i][1]) + this.poly[i][0]))
+            {
+                c = !c;
+            }
+        }
+
+        return c;
+    }
 });
 
 var GroundRenderer = Class.extend({
-    render: function(model,g,loc)
+    render: function(model, g, loc)
     {
-        var pts = calculateEllipse(0, 10, model.squareLength, model.straightLength, 0, model.sections);
+        var pts = model.poly;
         g.fillStyle = model.fillColour;
         g.beginPath();
         var pt = convertWorldToScreen([pts[0][0],pts[0][1]]);
@@ -27,7 +70,8 @@ var GroundRenderer = Class.extend({
         g.strokeStyle = "black";
         g.stroke();
 
-        var pts = calculateEllipse(0, 10, 26, 40, 0, 36);
+        // 30m circle
+        var pts = model.fieldingCirclePoly;
         g.beginPath();
         var pt = convertWorldToScreen([pts[0][0],pts[0][1]]);
         g.moveTo(pt[0],pt[1]);
@@ -56,27 +100,26 @@ function renderGroundBitmap()
     }
 }
 
-
 function calculateEllipse(x, y, a, b, angle, steps)
 {
-  var points = [];
+    var points = [];
 
-  // Angle is given by Degree Value
-  var beta = -angle * (Math.PI / 180); //(Math.PI/180) converts Degree Value into Radians
-  var sinbeta = Math.sin(beta);
-  var cosbeta = Math.cos(beta);
+    // Angle is given by Degree Value
+    var beta = -angle * (Math.PI / 180);
+    var sinbeta = Math.sin(beta);
+    var cosbeta = Math.cos(beta);
 
-  for (var i = 0; i < 360; i += 360 / steps)
-  {
-    var alpha = i * (Math.PI / 180) ;
-    var sinalpha = Math.sin(alpha);
-    var cosalpha = Math.cos(alpha);
+    for (var i = 0; i < 360; i += 360 / steps)
+    {
+        var alpha = i * (Math.PI / 180) ;
+        var sinalpha = Math.sin(alpha);
+        var cosalpha = Math.cos(alpha);
 
-    var X = x + (a * cosalpha * cosbeta - b * sinalpha * sinbeta);
-    var Y = y + (a * cosalpha * sinbeta + b * sinalpha * cosbeta);
+        var X = x + (a * cosalpha * cosbeta - b * sinalpha * sinbeta);
+        var Y = y + (a * cosalpha * sinbeta + b * sinalpha * cosbeta);
 
-    points.push([X, Y]);
-   }
+        points.push([X, Y]);
+    }
 
-  return points;
+    return points;
 }
